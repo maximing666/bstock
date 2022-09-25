4c64168a73572b349d164cc54fd12edbd9a99a3a
maximing666 <maximing666@126.com>
2022-09-26 00:52:21 +0800

    dupont.py

--- dupont.py
+++ dupont.py
@@ -7,14 +7,18 @@ import pandas as pd
 import datetime
 import configparser
 import pymysql
+from pymysql import IntegrityError
+import os
+
 
 #生成configparser对象
 config = configparser.ConfigParser()
 #读取配置文件
-# conffilename = r'E:\github\bstock\config\config.ini'
-conffilename = './config/config.ini'
+#conffilename = r'D:\github\bstock\config\config.ini'
+conffilename = './bstock/config/config.ini'
 config.read(conffilename, encoding='utf-8')
 #获取配置文件变量值
+print(config.sections())
 mysqlhost = config.get('mysql', 'host')
 mysqluser = config.get('mysql', 'user')
 mysqlpwd = config.get('mysql', 'password')
@@ -41,21 +45,33 @@ filetb = 'dupont'
 if filetb not in tbs: 
     sql="CREATE TABLE `"+mysqldb+"`.`%s`  (\
         `code` varchar(255) NOT NULL COMMENT '股票代码', \
+        `year` varchar(255) NOT NULL COMMENT '年', \
+        `quarter` varchar(255) NOT NULL COMMENT '季度', \
         `pubdate` date Default NULL COMMENT '发布日期', \
-        `statdate` date NOT NULL COMMENT '统计到最后一天日期', \
-        `dupontroe` double(20, 10) Default NULL COMMENT '净资产收益率',  \
-        `dupontassetstoequity` double(20, 10) Default NULL COMMENT '权益乘数,反映企业财务杠杆效应强弱和财务风险', \
-        `dupontassetturn` double(20, 10) Default NULL COMMENT '总资产周转率，反映企业资产管理效率的指标', \
-        `dupontpnitoni` double(20, 10) Default NULL COMMENT '归属母公司股东的净利润/净利润，反映母公司控股子公司百分比。如果企业追加投资，扩大持股比例，则本指标会增加。', \
-        `dupontnitogr` double(20, 10) Default NULL COMMENT '净利润/营业总收入，反映企业销售获利率', \
-        `duponttaxburden` double(20, 10) Default NULL COMMENT '净利润/利润总额，反映企业税负水平，该比值高则税负较低。净利润/利润总额=1-所得税/利润总额', \
-        `dupontintburden` double(20, 10) Default NULL COMMENT '利润总额/息税前利润，反映企业利息负担，该比值高则税负较低。利润总额/息税前利润=1-利息费用/息税前利润', \
-        `dupontebittogr` double(20, 10) Default NULL COMMENT '息税前利润/营业总收入，反映企业经营利润率，是企业经营获得的可供全体投资人（股东和债权人）分配的盈利占企业全部营收收入的百分比', \
-        PRIMARY KEY (`code`,`statdate`));"%(filetb)
+        `statdate` date Default NULL COMMENT '统计到最后一天日期', \
+        `dupontroe` decimal(15, 10)  DEFAULT 0 COMMENT '净资产收益率',  \
+        `dupontassetstoequity` decimal(15, 10)  DEFAULT 0 COMMENT '权益乘数,反映企业财务杠杆效应强弱和财务风险', \
+        `dupontassetturn` decimal(15, 10)  DEFAULT 0 COMMENT '总资产周转率，反映企业资产管理效率的指标', \
+        `dupontpnitoni` decimal(15, 10)  DEFAULT 0 COMMENT '归属母公司股东的净利润/净利润，反映母公司控股子公司百分比。如果企业追加投资，扩大持股比例，则本指标会增加。', \
+        `dupontnitogr` decimal(15, 10)  DEFAULT 0 COMMENT '净利润/营业总收入，反映企业销售获利率', \
+        `duponttaxburden` decimal(15, 10)  DEFAULT 0 COMMENT '净利润/利润总额，反映企业税负水平，该比值高则税负较低。净利润/利润总额=1-所得税/利润总额', \
+        `dupontintburden` decimal(15, 10)  DEFAULT 0 COMMENT '利润总额/息税前利润，反映企业利息负担，该比值高则税负较低。利润总额/息税前利润=1-利息费用/息税前利润', \
+        `dupontebittogr` decimal(15, 10)  DEFAULT 0 COMMENT '息税前利润/营业总收入，反映企业经营利润率，是企业经营获得的可供全体投资人（股东和债权人）分配的盈利占企业全部营收收入的百分比', \
+        PRIMARY KEY (`code`,`year`,`quarter`));"%(filetb)
     cur.execute(sql)
 else:
-    for i in ['dupont','sh.000001','sh.000002']:
-        tbs.remove(i)  
+    tbs.remove('dupont')
+
+i = 1
+while(i<1000):
+    k = str(i)        
+    j = 'sh.' + k.zfill(6)
+    try:
+        tbs.remove(j)  
+    except ValueError:
+        print('tbs list have no this value:', j)
+    finally:
+        i = i + 1
 
 # 登陆系统
 lg = bs.login()
@@ -68,8 +84,8 @@ for code in tbs:
     nowyear = datetime.date.today().year
     i = 2010
     while(i <= nowyear):
-        j = 1
-        while(j <= 4):
+        j = 4
+        while(j >= 1):
             # 查询杜邦指数
             dupont_list = []
             rs_dupont = bs.query_dupont_data(code=code, year=i, quarter=j)
@@ -77,58 +93,22 @@ for code in tbs:
                 dupont_list.append(rs_dupont.get_row_data())
             
             if len(dupont_list) > 0: 
-                print("-----", rs_dupont.fields) 
-                result_dupont = pd.DataFrame(dupont_list, columns=rs_dupont.fields)
-                # 打印输出
-                print(result_dupont)
-                sleep(111111)  
+                for m in dupont_list[0]:
+                    if m == '':
+                        dupont_list[0][dupont_list[0].index(m)] = 0
+                sql = "insert into `"+mysqldb+"`.`%s`(code,year,quarter,pubdate,statdate,dupontroe,dupontassetstoequity,dupontassetturn,dupontpnitoni,dupontnitogr,duponttaxburden,dupontintburden,dupontebittogr) values ('%s','%s','%s','%s','%s',%s,%s,%s,%s,%s,%s,%s,%s);"%(filetb,dupont_list[0][0],i,j,dupont_list[0][1],dupont_list[0][2],dupont_list[0][3],dupont_list[0][4],dupont_list[0][5],dupont_list[0][6],dupont_list[0][7],dupont_list[0][8],dupont_list[0][9],dupont_list[0][10])
+                print(sql)
+                try:
+                    cur.execute(sql)
+                except IntegrityError as duplicate_err:
+                    print("主键已存在。")
             else:
-                print(code, i, j, "Have no data..") 
-                i = i + 1
-                break
-            i = i + 1
-            j = j + 1
-
-
-
-sql="insert into `"+mysqldb+"`.`%s`(tdate,code,open,high,low,close,volume,amount,turn,tradestatus,pctchg,pettm) values ('%s','%s',%s,%s,%s,%s,%s,%s,%s,%s,%s,%s); "%(code,tdate,code,open,high,low,close,volume,amount,turn,tradestatus,pctchg,pettm)
-print(sql)
-sleep(100)
-
-
-try:
-    cur.execute(sql)
-except IntegrityError as duplicate_err:
-    print("主键已存在。")
-finally:
+                print(code, i, j, "Have no data..")
+                #break            
+            j = j - 1
+        i = i + 1
     connection.commit()
-
-print(codes)
-print("==========")
-sleep(100)
-
-# 登陆系统
-lg = bs.login()
-# 显示登陆返回信息
-print('login respond error_code:'+lg.error_code)
-print('login respond  error_msg:'+lg.error_msg)
-
-
-# 查询杜邦指数
-dupont_list = []
-rs_dupont = bs.query_dupont_data(code="sh.600316", year=2022, quarter=2)
-while (rs_dupont.error_code == '0') & rs_dupont.next():
-    dupont_list.append(rs_dupont.get_row_data())
-if len(dupont_list) > 0: 
-    print("-----", rs_dupont.fields)   
-else:
-    print("Have no data..") 
-    sleep(111111)   
-result_dupont = pd.DataFrame(dupont_list, columns=rs_dupont.fields)
-# 打印输出
-print(result_dupont)
-# 结果集输出到csv文件
-#result_dupont.to_csv("D:\\bstock\\download\\dupont\\dupont_data.csv", encoding="gbk", index=False)
-
+#关闭mysql连接
+connection.close()
 # 登出系统
 bs.logout()
