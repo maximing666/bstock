d215df94a4c0487c3b57609268e8ceccea7584e8
小马哥 <oncwnuLV4QoqUaCDeTOLyBObt50k@git.weixin.qq.com>
2023-02-24 00:59:01 +0800

    同步上两个季度growth数据

---
+++ crontask_growth.bat
@@ -0,0 +1,16 @@
+::获取当前时间
+set d=%date:~0,10%
+set t=%time:~0,8%
+echo %d% %t%
+::开启网卡
+netsh interface set interface "WLAN" enable
+ping -n 5 www.baidu.com
+::执行程序
+cd /d D:\github\bstock && conda activate bstock && python growth.py
+::
+::cd /d D:\github\bstock && conda activate bstock && python sendqywx.py
+::关闭网卡
+ping -n 5 www.baidu.com
+::netsh interface set interface "WLAN" disable
+::pause
+@cmd /k
--- growth.py
+++ growth.py
@@ -1,17 +1,19 @@
 # -*- coding: UTF-8 –*-
+#获取上两个季度成长能力数据
 from logging import exception
 from time import sleep
 from tkinter import SCROLL
 import baostock as bs
 import pandas as pd
 import datetime
+from dateutil.relativedelta import relativedelta
 import configparser
 import pymysql
 from pymysql import IntegrityError
 import os
 import t3
 
-t3.Logger()
+#t3.Logger()
 #生成configparser对象
 config = configparser.ConfigParser()
 #读取配置文件
@@ -60,6 +62,8 @@ if filetb not in tbs:
 else:
     tbs.remove('dupont')
     tbs.remove('growth')
+    tbs.remove('codeinfo')
+    tbs.remove('viewrecommend')
 i = 1
 while(i<1000):
     k = str(i)        
@@ -77,36 +81,43 @@ lg = bs.login()
 print('login respond error_code:'+lg.error_code)
 print('login respond  error_msg:'+lg.error_msg)
 
-nowyear = datetime.date.today().year
-i = nowyear
-while(i <= nowyear):
-    j = 3
-    while(j >= 1):
-        for code in tbs:
-            # 查询杜邦指数
-            growth_list = []
-            rs_growth = bs.query_growth_data(code=code, year=i, quarter=j)
-            while (rs_growth.error_code == '0') & rs_growth.next():
-                growth_list.append(rs_growth.get_row_data())
-            
-            if len(growth_list) > 0: 
-                for m in growth_list[0]:
-                    if m == '':
-                        growth_list[0][growth_list[0].index(m)] = 0
-                sql = "insert into `"+mysqldb+"`.`%s`(code,year,quarter,pubdate,statdate,yoyequity,yoyasset,yoyni,yoyepsbasic,yoypni) values ('%s','%s','%s','%s','%s',%s,%s,%s,%s,%s);"%(filetb,growth_list[0][0],i,j,growth_list[0][1],growth_list[0][2],growth_list[0][3],growth_list[0][4],growth_list[0][5],growth_list[0][6],growth_list[0][7])
-                print(sql)
-                try:
-                    cur.execute(sql)
-                except IntegrityError as duplicate_err:
-                    print("主键已存在。")
-            else:
-                print(code, i, j, "Have no data..")
-                #break 
-        connection.commit()           
-        j = j - 1
-    i = i + 1
+today = datetime.date.today()
+#3个月前的日期
+threemonthago = today + relativedelta(months=-3)
+#上个季度
+last_one_quarter = (threemonthago.month-1)//3 + 1
+#上个季度所在年份
+last_one_quarter_year = threemonthago.year
+#6个月前的日期
+sixmonthago = today + relativedelta(months=-6)
+#上两个个季度
+last_two_quarter = (sixmonthago.month-1)//3 + 1
+#上两个季度所在年份
+last_two_quarter_year = sixmonthago.year
+for i,j in [[last_one_quarter_year,last_one_quarter],[last_two_quarter_year,last_two_quarter]]:
+    for code in tbs:
+        # 查询指定季度成长能力
+        growth_list = []
+        rs_growth = bs.query_growth_data(code=code, year=i, quarter=j)
+        while (rs_growth.error_code == '0') & rs_growth.next():
+            growth_list.append(rs_growth.get_row_data())
+        print('growth_list:',growth_list)
+        if len(growth_list) > 0: 
+            print(i, j, code, "Have data. Starting...")
+            #获取code的数据有空数据，设置为0.
+            for m in growth_list[0]:
+                if m == '':
+                    growth_list[0][growth_list[0].index(m)] = 0
+            sql = "insert into `"+mysqldb+"`.`%s`(code,year,quarter,pubdate,statdate,yoyequity,yoyasset,    yoyni,yoyepsbasic,yoypni) values ('%s','%s','%s','%s','%s',%s,%s,%s,%s,%s)"%(filetb,    growth_list[0][0],i,j,growth_list[0][1],growth_list[0][2],growth_list[0][3],growth_list[0]  [4],growth_list[0][5],growth_list[0][6],growth_list[0][7])
+            print(sql)
+            try:
+                cur.execute(sql)
+            except IntegrityError as duplicate_err:
+                print("主键已存在。")
+        else:
+            print(i, j, code, "Have no data..")
+    connection.commit()           
     
-
 #关闭mysql连接
 connection.close()
 # 登出系统
