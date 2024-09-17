658c5e81755ed885e1fc57b69a1bdb72501ee911
maximing <maximing666@126.com>
2024-09-17 22:05:21 +0800

    1

--- crontask_growth.bat
+++ crontask_growth.bat
@@ -7,7 +7,7 @@ echo %d% %t%
 netsh interface set interface "WLAN" enable
 ping -n 5 www.baidu.com
 ::执行程序
-cd /d D:\github\bstock && conda activate bstock && python growth.py
+cd /e E:\github\bstock && conda activate bstock && python growth.py
 ::
 ::cd /d D:\github\bstock && conda activate bstock && python sendqywx.py
 ::关闭网卡
