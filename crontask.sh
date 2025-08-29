29a31503836c7c400c9171a0242505dab4eb7843
maximing <maximing666@126.com>
2025-08-29 20:39:57 +0800

    1

--- crontask.sh
+++ crontask.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 while true; do
-    now=$(date +%H%M%S)
+    now=$(date +%Y%m%d%H%M%S)
     echo "当前时间: $now"
-    if [ $now -ge "213000" ]; then  
+    now_hms=$(date +%H%M%S)  #时分秒
+    now_d=$(date +%d)        #日
+    # 如果当日时间大于等于21:30:00，则执行下载数据
+    if [[ "$now_hms" > "213000" ]] || [[ "$now_hms" == "213000" ]]; then 
         cd /opt/conda/bstock/ && pip install -r requirements_copy.txt
         echo "开始下载数据"
         python download_dayk_one.py
@@ -10,10 +13,18 @@ while true; do
         echo "开始写入数据"
         python write_dayk_one.py
         echo "写入完成:"$(date +%H%M%S)
+
+        if [[ "$now_d" == "01" || "$now_d" == "15" ]]; then
+            echo "开始执行growth.py"
+            python growth.py
+            echo "完成执行growth.py"
+        fi
+
         #sleep 9000使得时间到第二天。
         sleep 9000
     else
         echo "等待1800秒"
         sleep 1800
     fi
+
 done
--- result_days_up_one.py
+++ result_days_up_one.py
@@ -108,6 +108,7 @@ def put_viewrecommend():
     mysqluser = config.get('mysql', 'user')
     mysqlpwd = config.get('mysql', 'password')
     mysqldb = config.get('mysql', 'daykdb')
+    mysqlport = config.get('mysql', 'port')
 
 
     #连接MySQL数据库
@@ -115,6 +116,7 @@ def put_viewrecommend():
                                 user = mysqluser, #用户名 
                                 password = mysqlpwd,  #此处填登录数据库的密码
                                 db = mysqldb #数据库名
+                                port = mysqlport
                                 )
     #创建光标对象，一个连接可以有很多光标，一个光标跟踪一种数据状态。
     #光标对象作用是：、创建、删除、写入、查询等等
